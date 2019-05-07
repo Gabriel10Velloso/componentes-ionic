f1f083f50563064397e1d4b141d7e4c9a955b9d6
Gabriel10Velloso <gavelloso@gmail.com>
2019-05-07 08:38:52 -0300

    12 (Seção: 4) Componentes >> alterando menu - ion-split-pan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,25 @@
-import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Component } from '@angular/core';
+import { Componente } from './pages/interfaces/interfaces';
+import { Observable } from 'rxjs';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+    componentes: Observable<Componente[]>;
+    
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private dataService: DataService
   ) {
     this.initializeApp();
   }
@@ -21,6 +28,8 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      this.componentes = this.dataService.getMenuOptions();
     });
   }
 }
